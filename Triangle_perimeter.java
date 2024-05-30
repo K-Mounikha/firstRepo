4c0910b215328a069888f49d7db134e2a2902139
K-Mounikha <kovvadamounikha@gmail.com>
2024-05-30 17:43:20 +0530

    Assignment1

---
+++ Triangle_perimeter.java
@@ -0,0 +1,15 @@
+//Write a program to calculate the perimeter of a triangle having sides of length 2,3 and 5 units.
+
+package assignment1;
+
+public class Triangle_perimeter {
+
+	public static void main(String[] args) {
+		int s1=2,s2=3,s3=5,perimeter=0;
+        perimeter=s1+s2+s3;
+        System.out.println("perimeter of a triangle :"+perimeter);//10
+
+
+	}
+
+}
